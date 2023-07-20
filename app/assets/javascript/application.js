e6c7c2c722ccad5960b7a13109f909b28f6f457e
Jamessora <cadaciojerico@gmail.com>
2023-07-20 13:31:25 +0800

    import application.css

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -4,6 +4,7 @@ import "./controllers"
 
 import { Application } from "stimulus";
 import { definitionsFromContext } from "stimulus/webpack-helpers";
+import '../stylesheets/application.css'
 
 const application = Application.start();
 const context = require.context("../controllers", true, /\.js$/);
