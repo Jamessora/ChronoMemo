b1eb59163c85a72f5b80cd888ff5965ae9f98b75
Jamessora <cadaciojerico@example.com>
2025-05-17 18:48:43 +0800

    Update ruby and node version.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,11 +20,3 @@ console.log('Hello World from Webpacker')
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-
-import { Application } from "stimulus";
-import { definitionsFromContext } from "stimulus/webpack-helpers";
-import '../stylesheets/application.css'
-
-const application = Application.start();
-const context = require.context("../controllers", true, /\.js$/);
-application.load(definitionsFromContext(context));
